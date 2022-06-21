1376e10265186bb968b3abd48ee155c2227ee549
Sergey Ratiashvili <serg.s.r@gmail.com>
2022-06-21 17:02:44 +0300

    chore: simplier way to get the owner address

--- register-interchain-account.sh
+++ register-interchain-account.sh
@@ -1,6 +1,6 @@
 export DEMOWALLET_1=$(gaia-wasm-zoned keys show demowallet1 -a --keyring-backend test --home ./data/test-1) && echo $DEMOWALLET_1;
 export DEMOWALLET_2=$(gaia-wasm-zoned keys show demowallet2 -a --keyring-backend test --home ./data/test-2) && echo $DEMOWALLET_2;
-export OWNER=$(gaia-wasm-zoned keys list  --keyring-backend test --home ./data/test-1 --output json | jq -r -c '.[] | select(.name == "rly1") | .address') && echo $OWNER;
+export OWNER=$(gaia-wasm-zoned keys show rly1 -a --keyring-backend test --home ./data/test-1) && echo $OWNER;
 
 gaia-wasm-zoned tx interchaintxs register-interchain-account connection-0 $OWNER \
     --from $DEMOWALLET_1 --chain-id test-1 --home ./data/test-1 --node tcp://localhost:16657 --keyring-backend test -y
--- send_test_tx.sh
+++ send_test_tx.sh
@@ -1,5 +1,5 @@
-export OWNER=$(gaia-wasm-zoned keys list  --keyring-backend test --home ./data/test-1 --output json | jq -r -c '.[] | select(.name == "rly1") | .address') && echo $OWNER;
+export OWNER=$(gaia-wasm-zoned keys show rly1 -a --keyring-backend test --home ./data/test-1) && echo $OWNER;
 
 gaia-wasm-zoned tx interchaintxs submit-tx connection-0 $OWNER \
-    ./test.json --from demowallet1 --gas 10000000 --gas-adjustment 1.4 --gas-prices 0.5stake \
+    ./test_tx.json --from demowallet1 --gas 10000000 --gas-adjustment 1.4 --gas-prices 0.5stake \
     --broadcast-mode block --chain-id test-1 --keyring-backend test --home ./data/test-1 --node tcp://127.0.0.1:16657
