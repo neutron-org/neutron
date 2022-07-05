4d7ec81ce01c142014e866687333e079f302b2f7
swelf <golishkin@gmail.com>
2022-07-05 17:07:53 +0300

    added neutrond app

---
+++ cmd/neutrond/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"os"
+
+	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+	"github.com/neutron-org/neutron/app"
+)
+
+func main() {
+	rootCmd, _ := NewRootCmd()
+	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
+		os.Exit(1)
+	}
+}
