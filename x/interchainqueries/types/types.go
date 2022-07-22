179782983be1eb83819a4a8a7081bf3e126ad167
sotnikov-s <ssotnikov93@gmail.com>
2022-07-22 08:33:37 +0300

    add comments to the attribute related consts #LSC-75

--- x/interchainqueries/types/types.go
+++ x/interchainqueries/types/types.go
@@ -1,11 +1,21 @@
 package types
 
 const (
-	AttributeKeyQueryID         = "query_id"
-	AttributeKeyZoneID          = "zone_id"
-	AttributeKeyQueryType       = "type"
+	// AttributeKeyQueryID represents the key for event attribute delivering the query ID of a
+	// registered interchain query.
+	AttributeKeyQueryID = "query_id"
+	// AttributeKeyZoneID represents the key for event attribute delivering the zone ID where the
+	// event has been produced.
+	AttributeKeyZoneID = "zone_id"
+	// AttributeKeyQueryType represents the key for event attribute delivering the query type
+	// identifier (e.g. /cosmos.staking.v1beta1.Query/AllDelegations)
+	AttributeKeyQueryType = "type"
+	// AttributeKeyQueryParameters represents the key for event attribute delivering the parameters
+	// of an interchain query.
 	AttributeKeyQueryParameters = "parameters"
 
+	// AttributeValueCategory represents the value for the 'module' event attribute.
 	AttributeValueCategory = ModuleName
-	AttributeValueQuery    = "query"
+	// AttributeValueQuery represents the value for the 'action' event attribute.
+	AttributeValueQuery = "query"
 )
