5ea587442ef0c6b44d4c28bada95500c9ff2962c
Murad Karammaev <muradkarammaev@gmail.com>
2022-08-03 23:43:59 +0300

    fix: do not pass InterchainTxsKeeper twice

--- wasmbinding/wasm.go
+++ wasmbinding/wasm.go
@@ -4,13 +4,12 @@ import (
 	"github.com/CosmWasm/wasmd/x/wasm"
 	wasmkeeper "github.com/CosmWasm/wasmd/x/wasm/keeper"
 	interchainqueriesmodulekeeper "github.com/neutron-org/neutron/x/interchainqueries/keeper"
-	icacontrollerkeeper "github.com/neutron-org/neutron/x/interchaintxs/keeper"
 	interchaintransactionsmodulekeeper "github.com/neutron-org/neutron/x/interchaintxs/keeper"
 )
 
 // RegisterCustomPlugins returns wasmkeeper.Option that we can use to connect handlers for implemented custom queries and messages to the App
-func RegisterCustomPlugins(icaControllerKeeper *icacontrollerkeeper.Keeper, icqKeeper *interchainqueriesmodulekeeper.Keeper, ictxKeeper *interchaintransactionsmodulekeeper.Keeper) []wasmkeeper.Option {
-	wasmQueryPlugin := NewQueryPlugin(icaControllerKeeper, icqKeeper)
+func RegisterCustomPlugins(ictxKeeper *interchaintransactionsmodulekeeper.Keeper, icqKeeper *interchainqueriesmodulekeeper.Keeper) []wasmkeeper.Option {
+	wasmQueryPlugin := NewQueryPlugin(ictxKeeper, icqKeeper)
 
 	queryPluginOpt := wasmkeeper.WithQueryPlugins(&wasmkeeper.QueryPlugins{
 		Custom: CustomQuerier(wasmQueryPlugin),
