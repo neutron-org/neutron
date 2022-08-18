7cc01713d280f8bb04b2c4a24a02ec14b5041614
Sergey Ratiashvili <serg.s.r@gmail.com>
2022-08-18 16:42:36 +0300

    test: added tests

---
+++ x/interchainqueries/keeper/grpc_query_test.go
@@ -0,0 +1,50 @@
+package keeper_test
+
+import (
+	"fmt"
+
+	"github.com/neutron-org/neutron/x/interchainqueries/keeper"
+	iqtypes "github.com/neutron-org/neutron/x/interchainqueries/types"
+)
+
+func (suite *KeeperTestSuite) TestRemoteLastHeight() {
+	tests := []struct {
+		name string
+		run  func()
+	}{
+
+		{
+			"wrong connection id",
+			func() {
+				ctx := suite.ChainA.GetContext()
+				_, err := keeper.Keeper.LastRemoteHeight(suite.GetNeutronZoneApp(suite.ChainA).InterchainQueriesKeeper, ctx.Context(), &iqtypes.QueryLastRemoteHeight{ConnectionId: "test"})
+				suite.Require().Error(err)
+			},
+		},
+		{
+			"valid request",
+			func() {
+				ctx := suite.ChainA.GetContext()
+				_, err := keeper.Keeper.LastRemoteHeight(suite.GetNeutronZoneApp(suite.ChainA).InterchainQueriesKeeper, ctx.Context(), &iqtypes.QueryLastRemoteHeight{ConnectionId: "connection-0"})
+				suite.Require().NoError(err)
+			},
+		},
+		// {
+		// 	"valid request",
+		// 	func() {
+		// 		ctx := suite.ChainA.GetContext()
+		// 		_, err := keeper.Keeper.LastRemoteHeight(suite.GetNeutronZoneApp(suite.ChainA).InterchainQueriesKeeper, ctx.Context(), &iqtypes.QueryLastRemoteHeight{ConnectionId: "t"})
+		// 		// suite.Require().Greater(uint64(resp.Height), 0)
+		// 		suite.Require().NoError(err)
+		// 	},
+		// },
+	}
+
+	for i, tc := range tests {
+		tt := tc
+		suite.Run(fmt.Sprintf("Case %s, %d/%d tests", tt.name, i, len(tests)), func() {
+			suite.SetupTest()
+			tc.run()
+		})
+	}
+}
