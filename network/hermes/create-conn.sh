d85b6f90ab451e2fc21d84c2e225ba6efa97d7e7
Murad Karammaev <muradkarammaev@gmail.com>
2022-10-12 10:46:55 +0300

    chore: move 'create channel' Dockerfile → Hermes network scripts

--- network/hermes/create-conn.sh
+++ network/hermes/create-conn.sh
@@ -9,3 +9,4 @@ echo "Initiating connection handshake..."
 $HERMES_BINARY -c $CONFIG_DIR create connection test-1 test-2
 
 sleep 2
+hermes -c ./network/hermes/config.toml create channel --port-a transfer --port-b transfer test-1 connection-0
