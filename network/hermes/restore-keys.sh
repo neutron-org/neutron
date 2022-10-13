be2524161dc1980c0f75685c6472c3f4f1978eb1
nhpd <dmitry.klpv@gmail.com>
2022-10-13 16:58:52 +0400

    post-review fixes: refactor hermes key addition

--- network/hermes/restore-keys.sh
+++ network/hermes/restore-keys.sh
@@ -8,14 +8,10 @@ set -e
 sleep 1
 
 ### Restore Keys
-echo "alley afraid soup fall idea toss can goose become valve initial strong forward bright dish figure check leopard decide warfare hub unusual join cart" > ./test_1_m.txt
 $HERMES_BINARY --config ./network/hermes/config.toml keys delete --chain test-1 --all
-$HERMES_BINARY --config ./network/hermes/config.toml keys add --key-name testkey_1 --chain test-1 --mnemonic-file ./test_1_m.txt
-rm ./test_1_m.txt
+$HERMES_BINARY --config ./network/hermes/config.toml keys add --key-name testkey_1 --chain test-1 --mnemonic-file <(echo "alley afraid soup fall idea toss can goose become valve initial strong forward bright dish figure check leopard decide warfare hub unusual join cart")
 sleep 5
 
-echo "record gift you once hip style during joke field prize dust unique length more pencil transfer quit train device arrive energy sort steak upset" > ./test_2_m.txt
 $HERMES_BINARY --config ./network/hermes/config.toml keys delete --chain test-2 --all
-$HERMES_BINARY --config ./network/hermes/config.toml keys add --key-name testkey_2 --chain test-2 --mnemonic-file ./test_2_m.txt
-rm ./test_2_m.txt
+$HERMES_BINARY --config ./network/hermes/config.toml keys add --key-name testkey_2 --chain test-2 --mnemonic-file <(echo "record gift you once hip style during joke field prize dust unique length more pencil transfer quit train device arrive energy sort steak upset")
 sleep 5
