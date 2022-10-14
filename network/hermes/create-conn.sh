ad0d2486f958eec2bd30c9e2781b033573dc9c25
nhpd <dmitry.klpv@gmail.com>
2022-10-14 17:33:34 +0400

    post-rebase fixes

--- network/hermes/create-conn.sh
+++ network/hermes/create-conn.sh
@@ -9,4 +9,4 @@ echo "Initiating connection handshake..."
 $HERMES_BINARY --config $CONFIG_DIR create connection --a-chain test-1 --b-chain test-2
 
 sleep 2
-hermes -c ./network/hermes/config.toml create channel --port-a transfer --port-b transfer test-1 connection-0
+$HERMES_BINARY --config $CONFIG_DIR create channel --a-chain test-1 --a-connection connection-0 --a-port transfer --b-port transfer
