2cecfeb2ffdb112f9c1b82e3e01f19e14de5a692
Albert Andrejev <albert.andrejev@gmail.com>
2022-10-28 13:01:01 +0300

    Refactoring and timeout support

--- x/transfer/ibc_handlers.go
+++ x/transfer/ibc_handlers.go
@@ -11,6 +11,48 @@ import (
 	"github.com/neutron-org/neutron/x/interchaintxs/types"
 )
 
+const (
+	// We need to reserve this amount of gas on the context gas meter in order to add contract failure to keeper
+	GasReserve = 15000
+)
+
+func (im IBCModule) outOfGasRecovery(
+	ctx sdk.Context,
+	gasMeter sdk.GasMeter,
+	senderAddress sdk.AccAddress,
+	packet channeltypes.Packet,
+	data transfertypes.FungibleTokenPacketData,
+) {
+	if r := recover(); r != nil {
+		_, ok := r.(sdk.ErrorOutOfGas)
+		if !ok || !gasMeter.IsOutOfGas() {
+			panic(r)
+		}
+
+		im.keeper.Logger(ctx).Debug("Out of gas", "Gas meter", gasMeter.String(), "Packet data", data)
+		im.ContractmanagerKeeper.AddContractFailure(ctx, senderAddress.String(), packet.GetSequence(), "ack")
+
+	}
+}
+
+func (im IBCModule) createCachedContext(ctx sdk.Context) (cacheCtx sdk.Context, writeFn func(), newGasMeter sdk.GasMeter) {
+	gasLeft := ctx.GasMeter().Limit() - ctx.GasMeter().GasConsumed()
+	newLimit := gasLeft - GasReserve
+
+	if newLimit > gasLeft {
+		newLimit = 0
+	}
+
+	newGasMeter = sdk.NewGasMeter(newLimit)
+
+	cacheCtx, writeFn = ctx.CacheContext()
+	if strings.HasPrefix(ctx.GasMeter().String(), "BasicGasMeter") {
+		cacheCtx = ctx.WithGasMeter(newGasMeter)
+	}
+
+	return
+}
+
 // HandleAcknowledgement passes the acknowledgement data to the appropriate contract via a Sudo call.
 func (im IBCModule) HandleAcknowledgement(ctx sdk.Context, packet channeltypes.Packet, acknowledgement []byte) (err error) {
 
@@ -28,26 +70,8 @@ func (im IBCModule) HandleAcknowledgement(ctx sdk.Context, packet channeltypes.P
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "failed to decode address from bech32: %v", err)
 	}
 
-	// We need to reserver this amount of gas (15000) on the context gas meter in order to add contract failure to keeper
-	newGasMeter := sdk.NewGasMeter(ctx.GasMeter().Limit() - ctx.GasMeter().GasConsumed() - 15000)
-	defer func() {
-		if r := recover(); r != nil {
-			_, ok := r.(sdk.ErrorOutOfGas)
-			if !ok || !newGasMeter.IsOutOfGas() {
-				panic(r)
-			}
-
-			im.keeper.Logger(ctx).Debug("Out of gas", "Gas meter", newGasMeter.String(), "Packet data", data)
-			im.ContractmanagerKeeper.AddContractFailure(ctx, senderAddress.String(), packet.GetSequence(), "ack")
-
-			err = nil
-		}
-	}()
-
-	cacheCtx, writeFn := ctx.CacheContext()
-	if strings.HasPrefix(ctx.GasMeter().String(), "BasicGasMeter") {
-		cacheCtx = ctx.WithGasMeter(newGasMeter)
-	}
+	cacheCtx, writeFn, newGasMeter := im.createCachedContext(ctx)
+	defer im.outOfGasRecovery(ctx, newGasMeter, senderAddress, packet, data)
 
 	if ack.Success() {
 		_, err = im.sudoHandler.SudoResponse(cacheCtx, senderAddress, packet, ack.GetResult())
@@ -63,9 +87,9 @@ func (im IBCModule) HandleAcknowledgement(ctx sdk.Context, packet channeltypes.P
 		im.keeper.Logger(ctx).Debug("failed to Sudo contract on packet acknowledgement", err)
 	} else {
 		writeFn()
-		ctx.GasMeter().ConsumeGas(newGasMeter.GasConsumed(), "consume from cached context")
 	}
 
+	ctx.GasMeter().ConsumeGas(newGasMeter.GasConsumed(), "consume from cached context")
 	im.keeper.Logger(ctx).Debug("acknowledgement received", "Packet data", data, "CheckTx", ctx.IsCheckTx())
 
 	return nil
@@ -85,11 +109,18 @@ func (im IBCModule) HandleTimeout(ctx sdk.Context, packet channeltypes.Packet) e
 		return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "failed to decode address from bech32: %v", err)
 	}
 
-	_, err = im.sudoHandler.SudoTimeout(ctx, senderAddress, packet)
+	cacheCtx, writeFn, newGasMeter := im.createCachedContext(ctx)
+	defer im.outOfGasRecovery(ctx, newGasMeter, senderAddress, packet, data)
+
+	_, err = im.sudoHandler.SudoTimeout(cacheCtx, senderAddress, packet)
 	if err != nil {
+		im.ContractmanagerKeeper.AddContractFailure(ctx, senderAddress.String(), packet.GetSequence(), "timeout")
 		im.keeper.Logger(ctx).Debug("failed to Sudo contract on packet timeout", err)
-		return sdkerrors.Wrap(err, "failed to Sudo the contract on packet timeout")
+	} else {
+		writeFn()
 	}
+
+	ctx.GasMeter().ConsumeGas(newGasMeter.GasConsumed(), "consume from cached context")
 
 	return nil
 }
