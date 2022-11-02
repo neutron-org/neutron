51d4cbc60ffaf8bd9d8b72dfac75555283881b4a
Albert Andrejev <albert.andrejev@gmail.com>
2022-11-02 20:48:53 +0200

    fix paths

--- x/contractmanager/types/query.pb.gw.go
+++ x/contractmanager/types/query.pb.gw.go
@@ -339,11 +339,11 @@ func RegisterQueryHandlerClient(ctx context.Context, mux *runtime.ServeMux, clie
 }
 
 var (
-	pattern_Query_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"neutron-org", "neutron", "contractmanager", "params"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_Params_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"neutron", "contractmanager", "params"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Query_Failures_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3, 1, 0, 4, 1, 5, 4}, []string{"neutron-org", "neutron", "contractmanager", "failure", "address"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_Failures_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 1, 0, 4, 1, 5, 3}, []string{"neutron", "contractmanager", "failures", "address"}, "", runtime.AssumeColonVerbOpt(true)))
 
-	pattern_Query_AllFailures_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2, 2, 3}, []string{"neutron-org", "neutron", "contractmanager", "failure"}, "", runtime.AssumeColonVerbOpt(true)))
+	pattern_Query_AllFailures_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1, 2, 2}, []string{"neutron", "contractmanager", "failures"}, "", runtime.AssumeColonVerbOpt(true)))
 )
 
 var (
