45e9adbae5ba317b9f8d9e6d2def70ee1ad8c3f2
Andrei Zavgorodnii <andrei.z@p2p.org>
2022-11-22 23:58:03 +0200

    removed unnecessary UnpackInterfaces() call

--- wasmbinding/message_plugin.go
+++ wasmbinding/message_plugin.go
@@ -251,10 +251,6 @@ func (m *CustomMessenger) PerformSubmitTx(ctx sdk.Context, contractAddr sdk.AccA
 		})
 	}
 
-	if err := tx.UnpackInterfaces(m.Keeper.Codec); err != nil {
-		return nil, sdkerrors.Wrap(err, "failed to unpack interfaces to send interchain transaction")
-	}
-
 	if err := tx.ValidateBasic(); err != nil {
 		return nil, sdkerrors.Wrap(err, "failed to validate incoming SubmitTx message")
 	}
