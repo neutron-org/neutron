9986ba9849dedbceb4e4d815a63bc3e8d109184b
oldremez <83083413+oldremez@users.noreply.github.com>
2022-11-24 15:35:37 +0300

    post-review error formatting fixes

--- x/feerefunder/types/genesis.go
+++ x/feerefunder/types/genesis.go
@@ -24,30 +24,23 @@ func (gs GenesisState) Validate() error {
 	for _, info := range gs.FeeInfos {
 		addr, err := sdk.AccAddressFromBech32(info.Payer)
 		if err != nil {
-			return fmt.Errorf("failed to parse the payer address %v: %v", info.Payer, err)
+			return fmt.Errorf("failed to parse the payer address %s: %w", info.Payer, err)
 		}
 
 		if len(addr) != wasmtypes.ContractAddrLen {
-			return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "fee payer address %v is not a contract", info.Payer)
+			return sdkerrors.Wrapf(sdkerrors.ErrInvalidAddress, "fee payer address %s is not a contract", info.Payer)
 		}
 
 		if err := host.PortIdentifierValidator(info.PacketId.PortId); err != nil {
-			return fmt.Errorf("port id %v is invalid: %v", info.PacketId.PortId, err)
+			return fmt.Errorf("port id %s is invalid: %w", info.PacketId.PortId, err)
 		}
 
 		if err := host.ChannelIdentifierValidator(info.PacketId.ChannelId); err != nil {
-			return fmt.Errorf("channel id %v is invalid: %v", info.PacketId.PortId, err)
+			return fmt.Errorf("channel id %s is invalid: %w", info.PacketId.PortId, err)
 		}
 
 		if err := info.Fee.Validate(); err != nil {
-			return fmt.Errorf("invalid fees %v: %v", info.Fee, err)
-		}
-
-		if info.Fee.TimeoutFee.IsAllLT(gs.Params.MinFee.TimeoutFee) {
-			return sdkerrors.Wrapf(sdkerrors.ErrInsufficientFee, "provided timeout fee is less than min timeout fee: %v < %v", info.Fee.TimeoutFee, gs.Params.MinFee.TimeoutFee)
-		}
-		if info.Fee.AckFee.IsAllLT(gs.Params.MinFee.AckFee) {
-			return sdkerrors.Wrapf(sdkerrors.ErrInsufficientFee, "provided ack fee is less than min ack fee: %v < %v", info.Fee.AckFee, gs.Params.MinFee.AckFee)
+			return fmt.Errorf("invalid fees %s: %w", info.Fee, err)
 		}
 	}
 	return gs.Params.Validate()
