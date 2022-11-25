d2095565a71e01b4b2840f155b58be9bf08dbfb4
oldremez <83083413+oldremez@users.noreply.github.com>
2022-11-25 19:17:18 +0300

    tests refactoring

--- x/feerefunder/keeper/keeper_test.go
+++ x/feerefunder/keeper/keeper_test.go
@@ -1,50 +1,18 @@
-package keeper
+package keeper_test
 
 import (
-	"github.com/cosmos/cosmos-sdk/codec"
-	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
-	"github.com/cosmos/cosmos-sdk/store"
-	storetypes "github.com/cosmos/cosmos-sdk/store/types"
-	typesparams "github.com/cosmos/cosmos-sdk/x/params/types"
-	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
-	tmdb "github.com/tendermint/tm-db"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
+	testutil_keeper "github.com/neutron-org/neutron/testutil/keeper"
+	"github.com/pkg/errors"
 	"testing"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/neutron-org/neutron/x/feerefunder/types"
 	"github.com/stretchr/testify/require"
-	"github.com/tendermint/tendermint/libs/log"
 )
 
 func TestKeeperCheckFees(t *testing.T) {
-	storeKey := sdk.NewKVStoreKey(types.StoreKey)
-	memStoreKey := storetypes.NewMemoryStoreKey(types.MemStoreKey)
-
-	db := tmdb.NewMemDB()
-	stateStore := store.NewCommitMultiStore(db)
-	stateStore.MountStoreWithDB(storeKey, storetypes.StoreTypeIAVL, db)
-	stateStore.MountStoreWithDB(memStoreKey, storetypes.StoreTypeMemory, nil)
-	require.NoError(t, stateStore.LoadLatestVersion())
-
-	registry := codectypes.NewInterfaceRegistry()
-	cdc := codec.NewProtoCodec(registry)
-
-	paramsSubspace := typesparams.NewSubspace(cdc,
-		types.Amino,
-		storeKey,
-		memStoreKey,
-		"FeeParams",
-	)
-	k := NewKeeper(
-		cdc,
-		storeKey,
-		memStoreKey,
-		paramsSubspace,
-		nil,
-		nil,
-	)
-
-	ctx := sdk.NewContext(stateStore, tmproto.Header{}, false, log.NewNopLogger())
+	k, ctx := testutil_keeper.FeeKeeper(t)
 
 	k.SetParams(ctx, types.Params{
 		MinFee: types.Fee{
@@ -106,11 +74,12 @@ func TestKeeperCheckFees(t *testing.T) {
 		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
-			err := k.checkFees(ctx, *tc.fees)
+			err := k.CheckFees(ctx, *tc.fees)
 			if tc.valid {
 				require.NoError(t, err)
 			} else {
 				require.Error(t, err)
+				require.IsType(t, errors.WithStack(sdkerrors.ErrInsufficientFee), errors.Unwrap(err))
 			}
 		})
 	}
