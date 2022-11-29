f03abc51faf00aa3595676ad04f68a4740344c1f
Sergey Ratiashvili <serg.s.r@gmail.com>
2022-11-29 13:10:51 +0100

    fix: missing deposit array into registered query

--- wasmbinding/queries.go
+++ wasmbinding/queries.go
@@ -95,5 +95,6 @@ func mapGRPCRegisteredQueryToWasmBindings(grpcQuery types.RegisteredQuery) bindi
 		UpdatePeriod:                    grpcQuery.GetUpdatePeriod(),
 		LastSubmittedResultLocalHeight:  grpcQuery.GetLastSubmittedResultLocalHeight(),
 		LastSubmittedResultRemoteHeight: grpcQuery.GetLastSubmittedResultRemoteHeight(),
+		Deposit:                         grpcQuery.GetDeposit(),
 	}
 }
