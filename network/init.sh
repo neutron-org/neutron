94fc9c9e58110897d500dceece3a1939d479f0ba
quasisamurai <quazisamurai@gmail.com>
2022-11-30 16:31:04 +0400

    panic

--- network/init.sh
+++ network/init.sh
@@ -5,7 +5,10 @@ BINARY=neutrond
 CHAIN_DIR=./data
 CHAINID_1=test-1
 CHAINID_2=test-2
-DAO_CONTRACT=./contracts/neutron_dao.wasm
+DAO_CONTRACT=./contracts/cwd_core.wasm
+PROPOSAL_CONTRACT=./contracts/cwd_proposal_single.wasm
+VOTING_CONTRACT=./contracts/cwd_voting_vault.wasm
+STAKING_CONTRACT=./contracts/cwd_voting_native_staked.wasm
 VAL_MNEMONIC_1="clock post desk civil pottery foster expand merit dash seminar song memory figure uniform spice circle try happy obvious trash crime hybrid hood cushion"
 VAL_MNEMONIC_2="angry twist harsh drastic left brass behave host shove marriage fall update business leg direct reward object ugly security warm tuna model broccoli choice"
 DEMO_MNEMONIC_1="banner spread envelope side kite person disagree path silver will brother under couch edit food venture squirrel civil budget number acquire point work mass"
@@ -65,6 +68,44 @@ $BINARY add-genesis-account $($BINARY --home $CHAIN_DIR/$CHAINID_1 keys show dem
 $BINARY add-genesis-account $($BINARY --home $CHAIN_DIR/$CHAINID_1 keys show rly1 --keyring-backend test -a) 100000000000stake  --home $CHAIN_DIR/$CHAINID_1
 $BINARY add-genesis-account $($BINARY --home $CHAIN_DIR/$CHAINID_2 keys show rly2 --keyring-backend test -a) 100000000000stake  --home $CHAIN_DIR/$CHAINID_2
 
+echo "Initializing dao contract in genesis..."
+# Upload the dao contract
+$BINARY add-wasm-message store ${STAKING_CONTRACT} --output json --run-as neutron1m9l358xunhhwds0568za49mzhvuxx9ux8xafx2 --keyring-backend=test --home $CHAIN_DIR/$CHAINID_1
+#$BINARY add-wasm-message store ${DAO_CONTRACT} --output json  --run-as neutron1mjk79fjjgpplak5wq838w0yd982gzkyf8fxu8u --home $CHAIN_DIR/$CHAINID_1
+#$BINARY add-wasm-message store ${PROPOSAL_CONTRACT} --output json  --run-as neutron1mjk79fjjgpplak5wq838w0yd982gzkyf8fxu8u --home $CHAIN_DIR/$CHAINID_1
+#$BINARY add-wasm-message store ${VOTING_CONTRACT} --output json  --run-as neutron1mjk79fjjgpplak5wq838w0yd982gzkyf8fxu8u --home $CHAIN_DIR/$CHAINID_1
+# Instantiate the contract
+#INIT_CONTRACT="$(printf '{"owner":"%s"}' "${ADMIN_ADDRESS}")"
+INIT="$(printf '{"denom":"stake"}')"
+#TODO: fix quotes issue
+DAO_INIT="'""'"
+#echo "Instantiate"
+$BINARY add-wasm-message  instantiate-contract 1 ${INIT} --run-as demowallet1 --admin ${ADMIN_ADDRESS}  --label "staking"  --home $CHAIN_DIR/$CHAINID_1
+#$BINARY add-wasm-message  instantiate-contract 2 '{
+#                                                               "admin": null,
+#                                                               "automatically_add_cw20s": false,
+#                                                               "automatically_add_cw721s": false,
+#                                                               "description": "basic neutron dao",
+#                                                               "image_url": null,
+#                                                               "name": "Neutron",
+#                                                               "initial_items": null,
+#                                                               "proposal_modules_instantiate_info": [
+#                                                                 {
+#                                                                   "admin": null,
+#                                                                   "code_id": 3,
+#                                                                   "label": "DAO_Neutron_cw-proposal-single",
+#                                                                   "msg": "ewogICAgICAgICJhbGxvd19yZXZvdGluZyI6IGZhbHNlLAogICAgICAgICJwcmVfcHJvcG9zZV9pbmZvIjogewogICAgICAgICAgIkFueW9uZU1heVByb3Bvc2UiOiB7fQogICAgICAgIH0sCiAgICAgICAgImRlcG9zaXRfaW5mbyI6IG51bGwsCiAgICAgICAgImNsb3NlX3Byb3Bvc2FsX29uX2V4ZWN1dGlvbl9mYWlsdXJlIjogZmFsc2UsCiAgICAgICAgIm1heF92b3RpbmdfcGVyaW9kIjogewogICAgICAgICAgInRpbWUiOiA2MDQ4MDAKICAgICAgICB9LAogICAgICAgICJvbmx5X21lbWJlcnNfZXhlY3V0ZSI6IGZhbHNlLAogICAgICAgICJ0aHJlc2hvbGQiOiB7CiAgICAgICAgICAidGhyZXNob2xkX3F1b3J1bSI6IHsKICAgICAgICAgICAgInF1b3J1bSI6IHsKICAgICAgICAgICAgICAicGVyY2VudCI6ICIwLjIwIgogICAgICAgICAgICB9LAogICAgICAgICAgICAidGhyZXNob2xkIjogewogICAgICAgICAgICAgICJtYWpvcml0eSI6IHt9CiAgICAgICAgICAgIH0KICAgICAgICAgIH0KICAgICAgICB9CiAgICAgIH0="
+#                                                                 }
+#                                                               ],
+#                                                               "voting_module_instantiate_info": {
+#                                                                 "admin": null,
+#                                                                 "code_id": 4,
+#                                                                 "label": "DAO_Neutron_cwd-voting-vault",
+#                                                                 "msg": "ewogICAgICAibWFuYWdlciI6IG51bGwsCiAgICAgICJvd25lciI6IG51bGwsCiAgICAgICJzdGFraW5nIjogIm5ldXRyb24xNGhqMnRhdnE4ZnBlc2R3eHhjdTQ0cnR5M2hoOTB2aHVqcnZjbXN0bDR6cjN0eG1mdnc5czVjMmVwcSIKICAgIH0="
+#                                                               }
+#                                                       }' --run-as neutron1mjk79fjjgpplak5wq838w0yd982gzkyf8fxu8u --admin ${ADMIN_ADDRESS}  --label "DAO"  --home $CHAIN_DIR/$CHAINID_1
+
+
 echo "Add consumer section..."
 $BINARY add-consumer-section --home $CHAIN_DIR/$CHAINID_1
 $BINARY add-consumer-section --home $CHAIN_DIR/$CHAINID_2
@@ -99,4 +140,4 @@ sed -i -e 's/prometheus-retention-time = 0/prometheus-retention-time = 1000/g' $
 
 # Update host chain genesis to allow x/bank/MsgSend ICA tx execution
 sed -i -e 's/\"allow_messages\":.*/\"allow_messages\": [\"\/cosmos.bank.v1beta1.MsgSend\", \"\/cosmos.staking.v1beta1.MsgDelegate\", \"\/cosmos.staking.v1beta1.MsgUndelegate\"]/g' $CHAIN_DIR/$CHAINID_2/config/genesis.json
-sed -i -e 's/\"admins\":.*/\"admins\": [\"\/neutron14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s5c2epq\"]/g' $CHAIN_DIR/$CHAINID_1/config/genesis.json
+#sed -i -e 's/\"admins\":.*/\"admins\": [\"\/neutron14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s5c2epq\"]/g' $CHAIN_DIR/$CHAINID_1/config/genesis.json
