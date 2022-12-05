b27898ce377fd68f5499de2a8af92a717a2464cc
Albert Andrejev <albert.andrejev@gmail.com>
2022-12-05 12:35:57 +0200

    fix var

--- network/start.sh
+++ network/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NEUTRON_BINARY=neutrond
+NEUTROND_BINARY=neutrond
 GAIAD_BINARY=gaiad
 CHAIN_DIR=./data
 CHAINID_1=test-1
@@ -13,7 +13,7 @@ GRPCWEB_2=9091
 
 echo "Starting $CHAINID_1 in $CHAIN_DIR..."
 echo "Creating log file at $CHAIN_DIR/$CHAINID_1.log"
-$NEUTRON_BINARY start --log_level debug --log_format json --home $CHAIN_DIR/$CHAINID_1 --pruning=nothing --grpc.address="0.0.0.0:$GRPCPORT_1" --grpc-web.address="0.0.0.0:$GRPCWEB_1" --trace > $CHAIN_DIR/$CHAINID_1.log 2>&1 &
+$NEUTROND_BINARY start --log_level debug --log_format json --home $CHAIN_DIR/$CHAINID_1 --pruning=nothing --grpc.address="0.0.0.0:$GRPCPORT_1" --grpc-web.address="0.0.0.0:$GRPCWEB_1" --trace > $CHAIN_DIR/$CHAINID_1.log 2>&1 &
 
 echo "Starting gaiad chain $CHAINID_2 in $CHAIN_DIR..."
 echo "Creating log file at $CHAIN_DIR/$CHAINID_2.log"
