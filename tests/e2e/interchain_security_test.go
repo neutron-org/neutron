30f8737e778b45af50413207a474e5d09aadc26e
pr0n00gler <programmer10110@gmail.com>
2023-01-12 10:39:48 +0200

    Non-working tests comment

--- tests/e2e/interchain_security_test.go
+++ tests/e2e/interchain_security_test.go
@@ -18,7 +18,9 @@ func TestCCVTestSuite(t *testing.T) {
 	// Pass in concrete app types that implement the interfaces defined in /testutil/e2e/interfaces.go
 	ccvSuite := e2e.NewCCVTestSuite[*appProvider.App, *appConsumer.App](
 		// Pass in ibctesting.AppIniters for provider and consumer.
-		icssimapp.ProviderAppIniter, testutil.SetupTestingApp, []string{"TestSendRewardsRetries", "TestRewardsDistribution", "TestEndBlockRD"})
+		icssimapp.ProviderAppIniter, testutil.SetupTestingApp,
+		// TODO: These three tests just don't work in IS, so skip them for now
+		[]string{"TestSendRewardsRetries", "TestRewardsDistribution", "TestEndBlockRD"})
 
 	// Run tests
 	suite.Run(t, ccvSuite)
