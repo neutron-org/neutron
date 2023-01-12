4fe79caf2a907f4a79aa97279e15bfb2037ad005
pr0n00gler <programmer10110@gmail.com>
2023-01-12 20:01:27 +0200

    remove commented code

--- x/feeburner/client/cli/query.go
+++ x/feeburner/client/cli/query.go
@@ -2,13 +2,10 @@ package cli
 
 import (
 	"fmt"
-	// "strings"
 
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
-	// "github.com/cosmos/cosmos-sdk/client/flags"
-	// sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/neutron-org/neutron/x/feeburner/types"
 )
