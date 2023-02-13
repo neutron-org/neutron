1d8dc72c6bd46a43130a72b9b6eb95f97e8da0c3
sotnikov-s <ssotnikov93@gmail.com>
2023-02-13 16:47:19 +0300

    prettify ProcessBlock

--- x/interchainqueries/keeper/process_block_results.go
+++ x/interchainqueries/keeper/process_block_results.go
@@ -131,28 +131,28 @@ func (k Keeper) ProcessBlock(ctx sdk.Context, queryOwner sdk.AccAddress, queryID
 		txData = tx.GetData()
 		txHash = tmtypes.Tx(txData).Hash()
 	)
-	if !k.CheckTransactionIsAlreadyProcessed(ctx, queryID, txHash) {
-		// Check that cryptography is O.K. (tx is included in the block, tx was executed successfully)
-		if err = k.transactionVerifier.VerifyTransaction(tmHeader, tmNextHeader, tx); err != nil {
-			ctx.Logger().Debug("ProcessBlock: failed to verifyTransaction",
-				"error", err, "query_id", queryID, "tx_hash", hex.EncodeToString(txHash))
-			return sdkerrors.Wrapf(types.ErrInternal, "failed to verifyTransaction %s: %v", hex.EncodeToString(txHash), err)
-		}
-
-		// Let the query owner contract process the query result.
-		if _, err := k.contractManagerKeeper.SudoTxQueryResult(ctx, queryOwner, queryID, tmHeader.Header.Height, txData); err != nil {
-			ctx.Logger().Debug("ProcessBlock: failed to SudoTxQueryResult",
-				"error", err, "query_id", queryID, "tx_hash", hex.EncodeToString(txHash))
-			return sdkerrors.Wrapf(err, "contract %s rejected transaction query result (tx_hash: %s)",
-				queryOwner, hex.EncodeToString(txHash))
-		}
-
-		k.SaveTransactionAsProcessed(ctx, queryID, txHash)
-	} else {
+	if k.CheckTransactionIsAlreadyProcessed(ctx, queryID, txHash) {
 		ctx.Logger().Debug("ProcessBlock: transaction was already submitted",
 			"query_id", queryID, "tx_hash", hex.EncodeToString(txHash))
+		return nil
 	}
 
+	// Check that cryptography is O.K. (tx is included in the block, tx was executed successfully)
+	if err = k.transactionVerifier.VerifyTransaction(tmHeader, tmNextHeader, tx); err != nil {
+		ctx.Logger().Debug("ProcessBlock: failed to verifyTransaction",
+			"error", err, "query_id", queryID, "tx_hash", hex.EncodeToString(txHash))
+		return sdkerrors.Wrapf(types.ErrInternal, "failed to verifyTransaction %s: %v", hex.EncodeToString(txHash), err)
+	}
+
+	// Let the query owner contract process the query result.
+	if _, err := k.contractManagerKeeper.SudoTxQueryResult(ctx, queryOwner, queryID, tmHeader.Header.Height, txData); err != nil {
+		ctx.Logger().Debug("ProcessBlock: failed to SudoTxQueryResult",
+			"error", err, "query_id", queryID, "tx_hash", hex.EncodeToString(txHash))
+		return sdkerrors.Wrapf(err, "contract %s rejected transaction query result (tx_hash: %s)",
+			queryOwner, hex.EncodeToString(txHash))
+	}
+
+	k.SaveTransactionAsProcessed(ctx, queryID, txHash)
 	return nil
 }
 
