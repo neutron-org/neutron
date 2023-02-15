4ccfdd57e6ada50ae58afe36769516091815a812
oldremez <83083413+oldremez@users.noreply.github.com>
2023-02-15 23:14:21 +0200

    gaia denom fix

--- network/init-and-start-both.sh
+++ network/init-and-start-both.sh
@@ -25,7 +25,7 @@ export RESTPORT=1316
 export ROSETTA=9080
 export GRPCPORT=9090
 export GRPCWEB=9091
-export STAKEDENOM=stake
+export STAKEDENOM=uatom
 
 "$script_full_path"/init.sh
 "$script_full_path"/init-gaiad.sh
