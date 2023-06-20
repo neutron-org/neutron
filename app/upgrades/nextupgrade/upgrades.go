5df307cfe59b14c040a3a28b449a9321ffdeb6af
Albert Andrejev <albert.andrejev@gmail.com>
2023-06-20 15:35:58 +0300

    Remove debug output

--- app/upgrades/nextupgrade/upgrades.go
+++ app/upgrades/nextupgrade/upgrades.go
@@ -2,7 +2,6 @@ package nextupgrade
 
 import (
 	"errors"
-	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
@@ -17,7 +16,6 @@ func CreateUpgradeHandler(
 	keepers *upgrades.UpgradeKeepers,
 ) upgradetypes.UpgradeHandler {
 	return func(ctx sdk.Context, plan upgradetypes.Plan, vm module.VersionMap) (module.VersionMap, error) {
-		fmt.Println("CreateUpgradeHandler 1")
 		ctx.Logger().Info("Starting module migrations...")
 		vm, err := mm.RunMigrations(ctx, configurator, vm)
 		if err != nil {
