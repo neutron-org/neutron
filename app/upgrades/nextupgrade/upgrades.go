5120b4beb5363881920a4d453fde763e995cc370
Albert Andrejev <albert.andrejev@gmail.com>
2023-06-20 15:37:48 +0300

    Update log messages

--- app/upgrades/nextupgrade/upgrades.go
+++ app/upgrades/nextupgrade/upgrades.go
@@ -22,7 +22,7 @@ func CreateUpgradeHandler(
 			return vm, err
 		}
 
-		ctx.Logger().Info("Migrating GlobalFee Params...")
+		ctx.Logger().Info("Implementing GlobalFee Params...")
 
 		if !keepers.GlobalFeeSubspace.Has(ctx, types.ParamStoreKeyMinGasPrices) {
 			return vm, errors.New("minimum_gas_prices param not found")
@@ -34,13 +34,11 @@ func CreateUpgradeHandler(
 			sdk.NewDecCoinFromDec("ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9", sdk.MustNewDecFromStr("0.005")),
 			sdk.NewDecCoinFromDec("ibc/F082B65C88E4B6D5EF1DB243CDA1D331D002759E938A0F5CD3FFDC5D53B3E349", sdk.MustNewDecFromStr("0.05")),
 		}
-		ctx.Logger().Info("Empty min gas prices, set global fees")
-
 		requiredGlobalFees = requiredGlobalFees.Sort()
 
 		keepers.GlobalFeeSubspace.Set(ctx, types.ParamStoreKeyMinGasPrices, &requiredGlobalFees)
 
-		ctx.Logger().Info("global fees was set successfully")
+		ctx.Logger().Info("Global fees was set successfully")
 
 		ctx.Logger().Info("Upgrade complete")
 		return vm, err
