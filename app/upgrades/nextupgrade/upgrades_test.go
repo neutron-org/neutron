4f2a4d57364bdd1c00f7e7415793aa7e313f9002
Albert Andrejev <albert.andrejev@gmail.com>
2023-07-04 11:37:12 +0300

    Fix tests

--- app/upgrades/nextupgrade/upgrades_test.go
+++ app/upgrades/nextupgrade/upgrades_test.go
@@ -42,9 +42,9 @@ func (suite *UpgradeTestSuite) TestGlobalFeesUpgrade() {
 	globalFeeSubspace.Get(ctx, globalfeetypes.ParamStoreKeyMinGasPrices, &globalMinGasPrices)
 
 	requiredGlobalFees := sdk.DecCoins{
-		sdk.NewDecCoinFromDec("ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9", sdk.MustNewDecFromStr("0.005")),
-		sdk.NewDecCoinFromDec("ibc/F082B65C88E4B6D5EF1DB243CDA1D331D002759E938A0F5CD3FFDC5D53B3E349", sdk.MustNewDecFromStr("0.05")),
-		sdk.NewDecCoinFromDec("untrn", sdk.MustNewDecFromStr("0.01")),
+		sdk.NewDecCoinFromDec("ibc/C4CFF46FD6DE35CA4CF4CE031E643C8FDC9BA4B99AE598E9B0ED98FE3A2319F9", sdk.MustNewDecFromStr("0.026")),
+		sdk.NewDecCoinFromDec("ibc/F082B65C88E4B6D5EF1DB243CDA1D331D002759E938A0F5CD3FFDC5D53B3E349", sdk.MustNewDecFromStr("0.25")),
+		sdk.NewDecCoinFromDec("untrn", sdk.MustNewDecFromStr("0.9")),
 	}
 	suite.Require().Equal(requiredGlobalFees, globalMinGasPrices)
 }
