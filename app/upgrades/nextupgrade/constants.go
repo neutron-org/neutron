e4a00b79b7598cecafaaafe531893e4d0ada3096
swelf <golishkin@gmail.com>
2023-09-21 13:28:31 +0300

    added storeupgrades

--- app/upgrades/nextupgrade/constants.go
+++ app/upgrades/nextupgrade/constants.go
@@ -1,7 +1,11 @@
 package nextupgrade
 
 import (
+	"github.com/cosmos/cosmos-sdk/store/types"
+	consensusparamtypes "github.com/cosmos/cosmos-sdk/x/consensus/types"
+	crisistypes "github.com/cosmos/cosmos-sdk/x/crisis/types"
 	"github.com/neutron-org/neutron/app/upgrades"
+	buildertypes "github.com/skip-mev/pob/x/builder/types"
 )
 
 const (
@@ -12,4 +16,11 @@ const (
 var Upgrade = upgrades.Upgrade{
 	UpgradeName:          UpgradeName,
 	CreateUpgradeHandler: CreateUpgradeHandler,
+	StoreUpgrades: types.StoreUpgrades{
+		Added: []string{
+			consensusparamtypes.ModuleName,
+			crisistypes.ModuleName,
+			buildertypes.ModuleName,
+		},
+	},
 }
