eb8b5ae50907439ff9af0527a42ef0cb448a78b5
sotnikov-s <ssotnikov93@gmail.com>
2023-11-09 17:14:02 +0300

    move RedactError func to contractmanager keeper and make public

--- x/contractmanager/ibc_middleware.go
+++ x/contractmanager/ibc_middleware.go
@@ -3,12 +3,10 @@ package contractmanager
 import (
 	"fmt"
 
-	errorsmod "cosmossdk.io/errors"
-
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
-	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
 	"github.com/cometbft/cometbft/libs/log"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	contractmanagerkeeper "github.com/neutron-org/neutron/x/contractmanager/keeper"
 	contractmanagertypes "github.com/neutron-org/neutron/x/contractmanager/types"
 )
 
@@ -37,7 +35,12 @@ func (k SudoLimitWrapper) Sudo(ctx sdk.Context, contractAddress sdk.AccAddress, 
 		resp, err = k.WasmKeeper.Sudo(cacheCtx, contractAddress, msg)
 	}()
 	if err != nil { // the contract either returned an error or panicked with `out of gas`
-		failure := k.contractManager.AddContractFailure(ctx, contractAddress.String(), msg, redactError(err).Error())
+		failure := k.contractManager.AddContractFailure(
+			ctx,
+			contractAddress.String(),
+			msg,
+			contractmanagerkeeper.RedactError(err).Error(),
+		)
 		ctx.EventManager().EmitEvents(sdk.Events{
 			sdk.NewEvent(
 				wasmtypes.EventTypeSudo,
@@ -79,24 +82,4 @@ func createCachedContext(ctx sdk.Context, gasLimit uint64) (sdk.Context, func())
 	gasMeter := sdk.NewGasMeter(gasLimit)
 	cacheCtx = cacheCtx.WithGasMeter(gasMeter)
 	return cacheCtx, writeFn
-}
-
-// redactError removes non-determenistic details from the error returning just codespace and core
-// of the error. Returns full error for system errors.
-//
-// Copy+paste from https://github.com/neutron-org/wasmd/blob/5b59886e41ed55a7a4a9ae196e34b0852285503d/x/wasm/keeper/msg_dispatcher.go#L175-L190
-func redactError(err error) error {
-	// Do not redact system errors
-	// SystemErrors must be created in x/wasm and we can ensure determinism
-	if wasmvmtypes.ToSystemError(err) != nil {
-		return err
-	}
-
-	// FIXME: do we want to hardcode some constant string mappings here as well?
-	// Or better document them? (SDK error string may change on a patch release to fix wording)
-	// sdk/11 is out of gas
-	// sdk/5 is insufficient funds (on bank send)
-	// (we can theoretically redact less in the future, but this is a first step to safety)
-	codespace, code, _ := errorsmod.ABCIInfo(err, false)
-	return fmt.Errorf("codespace: %s, code: %d", codespace, code)
 }
--- x/contractmanager/keeper/failure.go
+++ x/contractmanager/keeper/failure.go
@@ -1,7 +1,10 @@
 package keeper
 
 import (
+	"fmt"
+
 	errorsmod "cosmossdk.io/errors"
+	wasmvmtypes "github.com/CosmWasm/wasmvm/types"
 	"github.com/cosmos/cosmos-sdk/store/prefix"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
@@ -13,7 +16,8 @@ import (
 //
 // WARNING: The errMsg string parameter is expected to be deterministic. It means that the errMsg
 // must be OS/library version agnostic and carry a concrete defined error message. One of the good
-// ways to do so is to redact error as it is done in SudoLimitWrapper Sudo method:
+// ways to do so is to redact error using the RedactError func as it is done in SudoLimitWrapper
+// Sudo method:
 // https://github.com/neutron-org/neutron/blob/4ee803ff75bb8fdddee1ed62fbf8b771d8006347/x/contractmanager/ibc_middleware.go#L40.
 // Another good way could be passing here some constant value.
 func (k Keeper) AddContractFailure(ctx sdk.Context, address string, sudoPayload []byte, errMsg string) types.Failure {
@@ -95,4 +99,24 @@ func (k Keeper) removeFailure(ctx sdk.Context, contractAddr sdk.AccAddress, id u
 	store := ctx.KVStore(k.storeKey)
 	failureKey := types.GetFailureKey(contractAddr.String(), id)
 	store.Delete(failureKey)
+}
+
+// RedactError removes non-determenistic details from the error returning just codespace and core
+// of the error. Returns full error for system errors.
+//
+// Copy+paste from https://github.com/neutron-org/wasmd/blob/5b59886e41ed55a7a4a9ae196e34b0852285503d/x/wasm/keeper/msg_dispatcher.go#L175-L190
+func RedactError(err error) error {
+	// Do not redact system errors
+	// SystemErrors must be created in x/wasm and we can ensure determinism
+	if wasmvmtypes.ToSystemError(err) != nil {
+		return err
+	}
+
+	// FIXME: do we want to hardcode some constant string mappings here as well?
+	// Or better document them? (SDK error string may change on a patch release to fix wording)
+	// sdk/11 is out of gas
+	// sdk/5 is insufficient funds (on bank send)
+	// (we can theoretically redact less in the future, but this is a first step to safety)
+	codespace, code, _ := errorsmod.ABCIInfo(err, false)
+	return fmt.Errorf("codespace: %s, code: %d", codespace, code)
 }
