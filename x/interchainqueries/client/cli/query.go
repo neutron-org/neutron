087754279be2142c89e1041f8c63908e9bf16e47
Jeremy Liu <31809888+NotJeremyLiu@users.noreply.github.com>
2023-12-29 18:27:08 -0700

    fix typo

--- x/interchainqueries/client/cli/query.go
+++ x/interchainqueries/client/cli/query.go
@@ -40,7 +40,7 @@ func GetQueryCmd(_ string) *cobra.Command {
 func CmdQueryRegisteredQuery() *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "registered-query [id]",
-		Short: "queries all the interchain queries in the module",
+		Short: "queries registered interchain query by id",
 		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx := client.GetClientContextFromCmd(cmd)
