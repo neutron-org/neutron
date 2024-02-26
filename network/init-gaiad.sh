81f57fc8f6220756d2e031c6b6a104fcf4c84e60
Murad Karammaev <muradkarammaev@gmail.com>
2024-02-26 09:48:59 +0200

    short unbonding time

--- network/init-gaiad.sh
+++ network/init-gaiad.sh
@@ -13,3 +13,4 @@ $BINARY gentx val1 "7000000000$STAKEDENOM" --home "$CHAIN_DIR" --chain-id "$CHAI
 $BINARY collect-gentxs --home "$CHAIN_DIR"
 
 sed -i -e 's/\"allow_messages\":.*/\"allow_messages\": [\"\/cosmos.bank.v1beta1.MsgSend\", \"\/cosmos.staking.v1beta1.MsgDelegate\", \"\/cosmos.staking.v1beta1.MsgUndelegate\"]/g' "$CHAIN_DIR/config/genesis.json"
+content="$(jq '.app_state.staking.params.unbonding_time = "360s"' "$CHAIN_DIR/config/genesis.json")" && echo -E "${content}" > "$CHAIN_DIR/config/genesis.json"
