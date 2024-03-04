ee93c41764db634d199b2c6efd345d368c08a8ab
pr0n00gler <programmer10110@gmail.com>
2024-03-04 18:55:41 +0200

    format

--- app/upgrades/v2.0.2/upgrades.go
+++ app/upgrades/v2.0.2/upgrades.go
@@ -7,6 +7,7 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	upgradetypes "github.com/cosmos/cosmos-sdk/x/upgrade/types"
+
 	"github.com/neutron-org/neutron/v2/app/upgrades"
 )
 
