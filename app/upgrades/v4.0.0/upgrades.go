4266bde0084bcfea8bc8a801efbe44fc37be596b
pr0n00gler <programmer10110@gmail.com>
2024-05-08 12:29:10 +0300

    enable vote extensions in upgrade handler

--- app/upgrades/v4.0.0/upgrades.go
+++ app/upgrades/v4.0.0/upgrades.go
@@ -4,8 +4,11 @@ import (
 	"context"
 	"fmt"
 
+	comettypes "github.com/cometbft/cometbft/proto/tendermint/types"
 	adminmoduletypes "github.com/cosmos/admin-module/x/adminmodule/types"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
+	consensuskeeper "github.com/cosmos/cosmos-sdk/x/consensus/keeper"
+	"github.com/cosmos/cosmos-sdk/x/consensus/types"
 
 	marketmapkeeper "github.com/skip-mev/slinky/x/marketmap/keeper"
 	marketmaptypes "github.com/skip-mev/slinky/x/marketmap/types"
@@ -40,6 +43,12 @@ func CreateUpgradeHandler(
 			return nil, err
 		}
 
+		ctx.Logger().Info("Setting consensus params...")
+		err = enableVoteExtensions(ctx, keepers.ConsensusKeeper)
+		if err != nil {
+			return nil, err
+		}
+
 		ctx.Logger().Info(fmt.Sprintf("Migration {%s} applied", UpgradeName))
 		return vm, nil
 	}
@@ -56,4 +65,25 @@ func setMarketMapParams(ctx sdk.Context, marketmapKeeper *marketmapkeeper.Keeper
 		Version:         params.Version,
 	}
 	return marketmapKeeper.SetParams(ctx, marketmapParams)
+}
+
+func enableVoteExtensions(ctx sdk.Context, consensusKeeper *consensuskeeper.Keeper) error {
+	oldParams, err := consensusKeeper.Params(ctx, &types.QueryParamsRequest{})
+	if err != nil {
+		return err
+	}
+
+	// we need to enable VoteExtensions for Slinky
+	oldParams.Params.Abci = &comettypes.ABCIParams{VoteExtensionsEnableHeight: ctx.BlockHeight()}
+
+	updateParamsMsg := types.MsgUpdateParams{
+		Authority: authtypes.NewModuleAddress(adminmoduletypes.ModuleName).String(),
+		Block:     oldParams.Params.Block,
+		Evidence:  oldParams.Params.Evidence,
+		Validator: oldParams.Params.Validator,
+		Abci:      oldParams.Params.Abci,
+	}
+
+	_, err = consensusKeeper.UpdateParams(ctx, &updateParamsMsg)
+	return err
 }
