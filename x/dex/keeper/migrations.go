7e9e4b11873690909fc4643cd98fa01901fa9a50
Julian Compagni Portis <jcompagni@gmail.com>
2024-05-13 12:10:03 -0700

    add params migration

---
+++ x/dex/keeper/migrations.go
@@ -0,0 +1,22 @@
+package keeper
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	v3 "github.com/neutron-org/neutron/v4/x/dex/migrations/v3"
+)
+
+// Migrator is a struct for handling in-place store migrations.
+type Migrator struct {
+	keeper Keeper
+}
+
+// NewMigrator returns a new Migrator.
+func NewMigrator(keeper Keeper) Migrator {
+	return Migrator{keeper: keeper}
+}
+
+// Migrate1to2 migrates from version 1 to 2.
+func (m Migrator) Migrate2to3(ctx sdk.Context) error {
+	return v3.MigrateStore(ctx, m.keeper.cdc, m.keeper.storeKey)
+}
