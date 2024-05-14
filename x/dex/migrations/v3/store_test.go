3f8d536d6ccc0cb25a369bc6ccd05a230a01309f
Julian Compagni Portis <jcompagni@gmail.com>
2024-05-14 11:33:21 -0700

    fix params migration test

--- x/dex/migrations/v3/store_test.go
+++ x/dex/migrations/v3/store_test.go
@@ -46,6 +46,5 @@ func (suite *V3DexMigrationTestSuite) TestParamsUpgrade() {
 	// Check params are correct
 	newParams := app.DexKeeper.GetParams(ctx)
 	suite.Require().EqualValues(oldParams.FeeTiers, newParams.FeeTiers)
-	suite.Require().EqualValues(newParams.GoodTilPurgeAllowance, types.DefaultGoodTilPurgeAllowance)
-	suite.Require().EqualValues(newParams.Max_JITsPerBlock, types.DefaultMaxJITsPerBlock)
+	suite.Require().EqualValues(newParams.Paused, types.DefaultPaused)
 }
