70d0aff69468cae2c812cdde8406e656ac4780c9
nhpd <dmitry.klpv@gmail.com>
2024-05-27 16:57:54 +0400

    add support for multiple nodes in add-consumer-section

--- cmd/neutrond/consumer.go
+++ cmd/neutrond/consumer.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"cosmossdk.io/errors"
 	"encoding/json"
 	"fmt"
-
-	"cosmossdk.io/errors"
+	"github.com/cometbft/cometbft/config"
+	"github.com/cometbft/cometbft/p2p"
+	"github.com/cometbft/cometbft/proto/tendermint/crypto"
+	"github.com/neutron-org/neutron/v4/testutil/consumer"
 
 	types1 "github.com/cometbft/cometbft/abci/types"
 	pvm "github.com/cometbft/cometbft/privval"
@@ -17,8 +20,6 @@ import (
 	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
 	ccvconsumertypes "github.com/cosmos/interchain-security/v4/x/ccv/consumer/types"
 	"github.com/spf13/cobra"
-
-	"github.com/neutron-org/neutron/v3/testutil/consumer"
 )
 
 func AddConsumerSectionCmd(defaultNodeHome string) *cobra.Command {
@@ -35,21 +36,35 @@ func AddConsumerSectionCmd(defaultNodeHome string) *cobra.Command {
 				serverCtx := server.GetServerContextFromCmd(cmd)
 				config := serverCtx.Config
 				config.SetRoot(clientCtx.HomeDir)
-				privValidator := pvm.LoadFilePV(config.PrivValidatorKeyFile(), config.PrivValidatorStateFile())
-				pk, err := privValidator.GetPubKey()
-				if err != nil {
-					return err
-				}
-				sdkPublicKey, err := cryptocodec.FromTmPubKeyInterface(pk)
-				if err != nil {
-					return err
-				}
-				tmProtoPublicKey, err := cryptocodec.ToTmProtoPublicKey(sdkPublicKey)
-				if err != nil {
-					return err
+
+				valDirs := []string{"./val_a", "./val_b", "./val_c", "./val_d"}
+
+				var initialValset []types1.ValidatorUpdate
+				var peerIds []string
+				for _, valDir := range valDirs {
+					privValidator := pvm.LoadFilePVEmptyState(valDir, "")
+					pk, err := privValidator.GetPubKey()
+					if err != nil {
+						return err
+					}
+					sdkPublicKey, err := cryptocodec.FromCmtPubKeyInterface(pk)
+					if err != nil {
+						return err
+					}
+					tmProtoPublicKey, err := cryptocodec.ToCmtProtoPublicKey(sdkPublicKey)
+					if err != nil {
+						return err
+					}
+					initialValset = append(initialValset, types1.ValidatorUpdate{PubKey: tmProtoPublicKey, Power: 25})
+
+					// ====== get peer ids ======
+					nodeKey, err := p2p.LoadNodeKey(valDir + "/config/node_key.json")
+					if err != nil {
+						return err
+					}
+					peerIds = append(peerIds, string(nodeKey.ID()))
 				}
 
-				initialValset := []types1.ValidatorUpdate{{PubKey: tmProtoPublicKey, Power: 100}}
 				vals, err := tmtypes.PB2TM.ValidatorUpdates(initialValset)
 				if err != nil {
 					return errors.Wrap(err, "could not convert val updates to validator set")
@@ -69,6 +84,58 @@ func AddConsumerSectionCmd(defaultNodeHome string) *cobra.Command {
 	flags.AddQueryFlagsToCmd(txCmd)
 
 	return txCmd
+}
+
+//func GetConsumerSectionCmd(defaultNodeHome string) *cobra.Command {
+//	genesisMutator := NewDefaultGenesisIO()
+//
+//	txCmd := &cobra.Command{
+//		Use:                        "save-validator-key",
+//		Short:                      "ONLY FOR TESTING PURPOSES! Gets public val key from home dir",
+//		SuggestionsMinimumDistance: 2,
+//		RunE: func(cmd *cobra.Command, _ []string) error {
+//			return genesisMutator.AlterConsumerModuleState(cmd, func(state *GenesisData, _ map[string]json.RawMessage) error {
+//				clientCtx := client.GetClientContextFromCmd(cmd)
+//				serverCtx := server.GetServerContextFromCmd(cmd)
+//				cfg := serverCtx.Config
+//				cfg.SetRoot(clientCtx.HomeDir)
+//				privValidator := pvm.LoadFilePV(cfg.PrivValidatorKeyFile(), cfg.PrivValidatorStateFile())
+//				pk, err := privValidator.GetPubKey()
+//				if err != nil {
+//					return nil
+//				}
+//				sdkPublicKey, err := cryptocodec.FromCmtPubKeyInterface(pk)
+//				filePV := pvm.LoadFilePVEmptyState("todo", "")
+//
+//				res := base64.StdEncoding.EncodeToString(sdkPublicKey.Bytes())
+//				fmt.Printf("public key in base64 format: %s\n", res)
+//
+//				return nil
+//			})
+//		},
+//	}
+//
+//	txCmd.Flags().String(flags.FlagHome, defaultNodeHome, "The application home directory")
+//	flags.AddQueryFlagsToCmd(txCmd)
+//
+//	return txCmd
+//}
+
+func extractValidatorKey(config *config.Config) (crypto.PublicKey, error) {
+	privValidator := pvm.LoadFilePV(config.PrivValidatorKeyFile(), config.PrivValidatorStateFile())
+	pk, err := privValidator.GetPubKey()
+	if err != nil {
+		return crypto.PublicKey{}, err
+	}
+	sdkPublicKey, err := cryptocodec.FromCmtPubKeyInterface(pk)
+	if err != nil {
+		return crypto.PublicKey{}, err
+	}
+	tmProtoPublicKey, err := cryptocodec.ToCmtProtoPublicKey(sdkPublicKey)
+	if err != nil {
+		return crypto.PublicKey{}, err
+	}
+	return tmProtoPublicKey, nil
 }
 
 type GenesisMutator interface {
