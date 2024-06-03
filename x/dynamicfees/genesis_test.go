e7e6e64b3a9ac686d411cde7464fce54431045cc
pr0n00gler <programmer10110@gmail.com>
2024-06-03 15:53:02 +0300

    genesis test

--- x/dynamicfees/genesis_test.go
+++ x/dynamicfees/genesis_test.go
@@ -1,31 +1,38 @@
 package dynamicfees_test
 
-//
-// import (
-//	"github.com/neutron-org/neutron/v4/x/feeburner"
-//	"testing"
-//
-//	"github.com/neutron-org/neutron/v4/app/config"
-//
-//	"github.com/stretchr/testify/require"
-//
-//	"github.com/neutron-org/neutron/v4/testutil/common/nullify"
-//	"github.com/neutron-org/neutron/v4/testutil/dynamicfees/keeper"
-//	"github.com/neutron-org/neutron/v4/x/dynamicfees/types"
-//)
-//
-// func TestGenesis(t *testing.T) {
-//	_ = config.GetDefaultConfig()
-//
-//	genesisState := types.GenesisState{
-//		Params: types.DefaultParams(),
-//	}
-//
-//	k, ctx := keeper.FeeburnerKeeper(t)
-//	feeburner.InitGenesis(ctx, *k, genesisState)
-//	got := feeburner.ExportGenesis(ctx, *k)
-//	require.NotNil(t, got)
-//
-//	nullify.Fill(&genesisState)
-//	nullify.Fill(got)
-//}
+import (
+	"testing"
+
+	"cosmossdk.io/math"
+	cosmostypes "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/neutron-org/neutron/v4/x/dynamicfees"
+
+	"github.com/neutron-org/neutron/v4/app/config"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/neutron-org/neutron/v4/testutil/common/nullify"
+	"github.com/neutron-org/neutron/v4/testutil/dynamicfees/keeper"
+	"github.com/neutron-org/neutron/v4/x/dynamicfees/types"
+)
+
+func TestGenesis(t *testing.T) {
+	_ = config.GetDefaultConfig()
+
+	params := types.DefaultParams()
+	params.NtrnPrices = append(params.NtrnPrices, cosmostypes.DecCoin{Denom: "uatom", Amount: math.LegacyMustNewDecFromStr("10")})
+
+	genesisState := types.GenesisState{
+		Params: params,
+	}
+
+	k, ctx := keeper.DynamicFeesKeeper(t)
+	dynamicfees.InitGenesis(ctx, *k, genesisState)
+	got := dynamicfees.ExportGenesis(ctx, *k)
+	require.NotNil(t, got)
+	require.Equal(t, genesisState, *got)
+
+	nullify.Fill(&genesisState)
+	nullify.Fill(got)
+}
