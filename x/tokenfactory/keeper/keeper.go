0da01d17cdae69aa38320e679418c929a342d2f8
Andrei Zavgorodnii <andrei.z@hadronlabs.org>
2024-06-13 16:35:43 +0100

    fix: add tf checks & tests

--- x/tokenfactory/keeper/keeper.go
+++ x/tokenfactory/keeper/keeper.go
@@ -3,13 +3,12 @@ package keeper
 import (
 	"context"
 	"fmt"
+	"sort"
 
 	"cosmossdk.io/log"
-	"github.com/cosmos/cosmos-sdk/codec"
-	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
-
 	"cosmossdk.io/store/prefix"
 	storetypes "cosmossdk.io/store/types"
+	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/neutron-org/neutron/v4/x/tokenfactory/types"
@@ -17,13 +16,13 @@ import (
 
 type (
 	Keeper struct {
-		storeKey       storetypes.StoreKey
-		permAddrs      map[string]authtypes.PermissionsForAddress
-		cdc            codec.Codec
-		accountKeeper  types.AccountKeeper
-		bankKeeper     types.BankKeeper
-		contractKeeper types.ContractKeeper
-		authority      string
+		storeKey             storetypes.StoreKey
+		knownModuleAddresses []string
+		cdc                  codec.Codec
+		accountKeeper        types.AccountKeeper
+		bankKeeper           types.BankKeeper
+		contractKeeper       types.ContractKeeper
+		authority            string
 	}
 )
 
@@ -37,19 +36,20 @@ func NewKeeper(
 	contractKeeper types.ContractKeeper,
 	authority string,
 ) Keeper {
-	permAddrs := make(map[string]authtypes.PermissionsForAddress)
-	for name, perms := range maccPerms {
-		permAddrs[name] = authtypes.NewPermissionsForAddress(name, perms)
+	sortedKnownModuleAddresses := make([]string, 0, len(maccPerms))
+	for moduleName := range maccPerms {
+		sortedKnownModuleAddresses = append(sortedKnownModuleAddresses, moduleName)
 	}
+	sort.Strings(sortedKnownModuleAddresses)
 
 	return Keeper{
-		cdc:            cdc,
-		storeKey:       storeKey,
-		permAddrs:      permAddrs,
-		accountKeeper:  accountKeeper,
-		bankKeeper:     bankKeeper,
-		contractKeeper: contractKeeper,
-		authority:      authority,
+		cdc:                  cdc,
+		storeKey:             storeKey,
+		knownModuleAddresses: sortedKnownModuleAddresses,
+		accountKeeper:        accountKeeper,
+		bankKeeper:           bankKeeper,
+		contractKeeper:       contractKeeper,
+		authority:            authority,
 	}
 }
 
