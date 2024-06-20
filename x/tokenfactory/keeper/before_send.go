b5d02d424ccd054e95d2ed2be292bb868258e63a
Julian Compagni Portis <jcompagni@gmail.com>
2024-06-20 12:56:29 -0400

    PR review fixes

--- x/tokenfactory/keeper/before_send.go
+++ x/tokenfactory/keeper/before_send.go
@@ -68,6 +68,27 @@ func (k Keeper) Hooks() Hooks {
 	return Hooks{k}
 }
 
+func (k Keeper) AssertIsHookWhitelisted(ctx sdk.Context, denom string, contractAddress sdk.AccAddress) error {
+	contractInfo := k.contractKeeper.GetContractInfo(ctx, contractAddress)
+	if contractInfo == nil {
+		return types.ErrBeforeSendHookNotWhitelisted.Wrapf("contract with address (%s) does not exist", contractAddress.String())
+	}
+	codeID := contractInfo.CodeID
+	whitelistedHooks := k.GetParams(ctx).WhitelistedHooks
+	denomCreator, _, err := types.DeconstructDenom(denom)
+	if err != nil {
+		return types.ErrBeforeSendHookNotWhitelisted.Wrapf("invalid denom: %s", denom)
+	}
+
+	for _, hook := range whitelistedHooks {
+		if hook.CodeID == codeID && hook.DenomCreator == denomCreator {
+			return nil
+		}
+	}
+
+	return types.ErrBeforeSendHookNotWhitelisted.Wrapf("no whitelist for contract with codeID (%d) and denomCreator (%s) ", codeID, denomCreator)
+}
+
 // TrackBeforeSend calls the before send listener contract suppresses any errors
 func (h Hooks) TrackBeforeSend(ctx context.Context, from, to sdk.AccAddress, amount sdk.Coins) {
 	_ = h.k.callBeforeSendListener(ctx, from, to, amount, false)
@@ -100,8 +121,11 @@ func (k Keeper) callBeforeSendListener(ctx context.Context, from, to sdk.AccAddr
 			}
 
 			// Do not invoke hook if denom is not whitelisted
+			// NOTE: hooks must already be whitelisted before they can be added, so under normal operation this check should never fail.
+			// It is here as an emergency override if we want to shutoff a hook. We do not return the error because once it is removed from the whitelist
+			// a hook should not be able to block a send.
 			if err := k.AssertIsHookWhitelisted(c, coin.Denom, cwAddr); err != nil {
-				return nil
+				continue
 			}
 
 			var msgBz []byte
--- x/tokenfactory/keeper/params.go
+++ x/tokenfactory/keeper/params.go
@@ -29,24 +29,3 @@ func (k Keeper) SetParams(ctx sdk.Context, params types.Params) error {
 	store.Set(types.ParamsKey, bz)
 	return nil
 }
-
-func (k Keeper) AssertIsHookWhitelisted(ctx sdk.Context, denom string, contractAddress sdk.AccAddress) error {
-	contractInfo := k.contractKeeper.GetContractInfo(ctx, contractAddress)
-	if contractInfo == nil {
-		return types.ErrBeforeSendHookNotWhitelisted.Wrapf("contract with address (%s) does not exist", contractAddress.String())
-	}
-	codeID := contractInfo.CodeID
-	whitelistedHooks := k.GetParams(ctx).WhitelistedHooks
-	denomCreator, _, err := types.DeconstructDenom(denom)
-	if err != nil {
-		return types.ErrBeforeSendHookNotWhitelisted.Wrapf("invalid denom: %s", denom)
-	}
-
-	for _, hook := range whitelistedHooks {
-		if hook.CodeID == codeID && hook.DenomCreator == denomCreator {
-			return nil
-		}
-	}
-
-	return types.ErrBeforeSendHookNotWhitelisted.Wrapf("no whitelist for contract with codeID (%d) and denomCreator (%s) ", codeID, denomCreator)
-}
