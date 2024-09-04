6b9ddc2bdc7cd18852c78fda942f41da311ac7d6
pr0n00gler <programmer10110@gmail.com>
2024-09-04 15:04:14 +0300

    check execution stage in tests

--- wasmbinding/test/custom_message_test.go
+++ wasmbinding/test/custom_message_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	contractmanagertypes "github.com/neutron-org/neutron/v4/x/contractmanager/types"
+	types2 "github.com/neutron-org/neutron/v4/x/cron/types"
 
 	"cosmossdk.io/math"
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
@@ -720,6 +721,10 @@ func (suite *CustomMessengerTestSuite) TestAddRemoveSchedule() {
 			Name: "schedule1",
 		},
 	}
+
+	schedule, ok := suite.neutron.CronKeeper.GetSchedule(suite.ctx, "schedule1")
+	suite.True(ok)
+	suite.Equal(types2.ExecutionStage_EXECUTION_STAGE_END_BLOCKER, schedule.ExecutionStage)
 
 	// Dispatch AddSchedule message
 	_, err = suite.executeNeutronMsg(suite.contractAddress, msg)
