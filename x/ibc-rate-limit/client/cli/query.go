166daf286c059e29d171666187bb7c487d5cfe05
quasisamurai <quazisamurai@gmail.com>
2024-09-19 10:20:20 -0300

    uncomment cli

--- x/ibc-rate-limit/client/cli/query.go
+++ x/ibc-rate-limit/client/cli/query.go
@@ -1,50 +1,56 @@
 package cli
 
 import (
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/neutron-org/neutron/v4/x/ibc-rate-limit/client/queryproto"
+	"github.com/neutron-org/neutron/v4/x/ibc-rate-limit/types"
 	"github.com/spf13/cobra"
 )
 
 // GetQueryCmd returns the cli query commands for this module
 func GetQueryCmd() *cobra.Command {
 	// Group ibc-rate-limit queries under a subcommand
-	// cmd := &cobra.Command{
-	//	Use:                        types.ModuleName,
-	//	Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
-	//	DisableFlagParsing:         true,
-	//	SuggestionsMinimumDistance: 2,
-	//	RunE:                       client.ValidateCmd,
-	// }
+	cmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
 
-	// cmd.AddCommand(
-	//	GetParams(),
-	//)
+	cmd.AddCommand(
+		GetParams(),
+	)
 
 	return nil
 }
 
 // GetParams returns the params for the module
 func GetParams() *cobra.Command {
-	// cmd := &cobra.Command{
-	//	Use:   "params [flags]",
-	//	Short: "Get the params for the x/ibc-rate-limit module",
-	//	Args:  cobra.ExactArgs(0),
-	//	RunE: func(cmd *cobra.Command, _ []string) error {
-	//		clientCtx, err := client.GetClientQueryContext(cmd)
-	//		if err != nil {
-	//			return err
-	//		}
-	//		queryClient := types.NewQueryClient(clientCtx)
-	//
-	//		res, err := queryClient.Params(cmd.Context(), &types.QueryParamsRequest{})
-	//		if err != nil {
-	//			return err
-	//		}
-	//
-	//		return clientCtx.PrintProto(res)
-	//	},
-	// }
-	//
-	// flags.AddQueryFlagsToCmd(cmd)
+	cmd := &cobra.Command{
+		Use:   "params [flags]",
+		Short: "Get the params for the x/ibc-rate-limit module",
+		Args:  cobra.ExactArgs(0),
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx, err := client.GetClientQueryContext(cmd)
+			if err != nil {
+				return err
+			}
+			queryClient := queryproto.NewQueryClient(clientCtx)
+
+			res, err := queryClient.Params(cmd.Context(), &queryproto.ParamsRequest{})
+			if err != nil {
+				return err
+			}
+
+			return clientCtx.PrintProto(res)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
 
 	return nil
 }
