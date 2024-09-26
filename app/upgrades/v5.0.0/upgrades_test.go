dc65431f8b1af8b6c20c2d2b77ce143b031bd56c
pr0n00gler <programmer10110@gmail.com>
2024-09-26 16:19:35 +0300

    update market map admin

--- app/upgrades/v5.0.0/upgrades_test.go
+++ app/upgrades/v5.0.0/upgrades_test.go
@@ -5,6 +5,7 @@ import (
 
 	"cosmossdk.io/math"
 	upgradetypes "cosmossdk.io/x/upgrade/types"
+	"github.com/stretchr/testify/require"
 	"github.com/stretchr/testify/suite"
 
 	v500 "github.com/neutron-org/neutron/v5/app/upgrades/v5.0.0"
@@ -25,6 +26,25 @@ func TestKeeperTestSuite(t *testing.T) {
 
 func (suite *UpgradeTestSuite) SetupTest() {
 	suite.IBCConnectionTestSuite.SetupTest()
+}
+
+func (suite *UpgradeTestSuite) TestOracleUpgrade() {
+	app := suite.GetNeutronZoneApp(suite.ChainA)
+	ctx := suite.ChainA.GetContext()
+	t := suite.T()
+
+	upgrade := upgradetypes.Plan{
+		Name:   v500.UpgradeName,
+		Info:   "some text here",
+		Height: 100,
+	}
+	require.NoError(t, app.UpgradeKeeper.ApplyUpgrade(ctx, upgrade))
+
+	params, err := app.MarketMapKeeper.GetParams(ctx)
+	suite.Require().NoError(err)
+	suite.Require().Equal(params.MarketAuthorities[0], "neutron1hxskfdxpp5hqgtjj6am6nkjefhfzj359x0ar3z")
+	suite.Require().Equal(params.MarketAuthorities[1], v500.MarketMapAuthorityMultisig)
+	suite.Require().Equal(params.Admin, "neutron1hxskfdxpp5hqgtjj6am6nkjefhfzj359x0ar3z")
 }
 
 func (suite *UpgradeTestSuite) TestUpgradeDexPause() {
