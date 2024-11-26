732185c8a07ca6ec1b1525a61a7c031dcb77252f
swelf <golishkin@gmail.com>
2024-11-26 18:49:31 +0300

    inital commit

---
+++ x/revenue/genesis_test.go
@@ -0,0 +1,27 @@
+package revenue_test
+
+//func TestGenesis(t *testing.T) {
+//	k, ctx := keeper.CronKeeper(t, nil, nil)
+//
+//	genesisState := types.GenesisState{
+//		Params: types.DefaultParams(),
+//		ScheduleList: []types.Schedule{
+//			{
+//				Name:              "a",
+//				Period:            5,
+//				Msgs:              nil,
+//				LastExecuteHeight: uint64(ctx.BlockHeight()),
+//			},
+//		},
+//	}
+//
+//	cron.InitGenesis(ctx, *k, genesisState)
+//	got := cron.ExportGenesis(ctx, *k)
+//	require.NotNil(t, got)
+//
+//	nullify.Fill(&genesisState)
+//	nullify.Fill(got)
+//
+//	require.Equal(t, genesisState.Params, got.Params)
+//	require.ElementsMatch(t, genesisState.ScheduleList, got.ScheduleList)
+//}
---
+++ x/revenue/keeper/msg_server.go
@@ -0,0 +1,22 @@
+package keeper
+
+import (
+	"context"
+	"github.com/neutron-org/neutron/v5/x/revenue/types"
+)
+
+type msgServer struct {
+	keeper Keeper
+}
+
+// NewMsgServerImpl returns an implementation of the MsgServer interface
+// for the provided Keeper.
+func NewMsgServerImpl(keeper Keeper) types.MsgServer {
+	return &msgServer{keeper: keeper}
+}
+
+var _ types.MsgServer = msgServer{}
+
+func (s msgServer) UpdateParams(context context.Context, msg *types.MsgUpdateParams) (*types.MsgUpdateParamsResponse, error) {
+	return nil, nil
+}
