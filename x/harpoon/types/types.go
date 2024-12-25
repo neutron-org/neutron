ea93c57275772c8efd3f166ae36f4e40775e009b
nhpd <dmitry.klpv@gmail.com>
2024-12-26 01:05:05 +0800

    add hook msg

--- x/harpoon/types/types.go
+++ x/harpoon/types/types.go
@@ -1,1 +1,14 @@
 package types
+
+//// HookType represents all possible hooks in the staking module (same as in protobuf above)
+//type HookType int
+//
+//// TODO: need it?
+//const (
+//	HookUnspecified                HookType = iota // 0 - Unspecified or uninitialized hook
+//	BeforeDelegationCreated                        // 1 - Triggered before a delegation is created
+//	BeforeDelegationSharesModified                 // 2 - Triggered before delegation shares are modified (e.g. undelegation)
+//	BeforeDelegationRemoved                        // 3 - Triggered before a delegation is completely removed
+//	BeforeRedelegationCreated                      // 4 - Triggered before a redelegation is created
+//	BeforeValidatorSlashed                         // 5 - Triggered before a validator is slashed
+//)
