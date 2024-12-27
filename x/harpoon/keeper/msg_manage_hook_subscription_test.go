151065a7c32935cf47bde39415a88458e1303d91
nhpd <dmitry.klpv@gmail.com>
2024-12-27 16:28:51 +0800

    passing test

--- x/harpoon/keeper/msg_manage_hook_subscription_test.go
+++ x/harpoon/keeper/msg_manage_hook_subscription_test.go
@@ -2,18 +2,13 @@ package keeper_test
 
 import (
 	"github.com/golang/mock/gomock"
+	"github.com/neutron-org/neutron/v5/testutil"
 	testutil_keeper "github.com/neutron-org/neutron/v5/testutil/harpoon/keeper"
+	mock_types "github.com/neutron-org/neutron/v5/testutil/mocks/harpoon/types"
 	"github.com/neutron-org/neutron/v5/x/harpoon/keeper"
 	"github.com/neutron-org/neutron/v5/x/harpoon/types"
-
-	//"context"
-	mock_types "github.com/neutron-org/neutron/v5/testutil/mocks/harpoon/types"
 	"github.com/stretchr/testify/require"
 	"testing"
-	//
-	//keepertest "github.com/neutron-org/neutron/v5/testutil/harpoon/keeper"
-	//"github.com/neutron-org/neutron/v5/x/harpoon/keeper"
-	//"github.com/neutron-org/neutron/v5/x/harpoon/types"
 )
 
 func TestManageHookSubscription(t *testing.T) {
@@ -27,15 +22,55 @@ func TestManageHookSubscription(t *testing.T) {
 
 	msgServer := keeper.NewMsgServerImpl(*k)
 
-	// empty authority
-	// incorrect authority
-	// invalid hook subscription message
-	// good case
+	tests := []struct {
+		name                      string
+		manageHookSubscriptionMsg types.MsgManageHookSubscription
+		expectedErr               string
+	}{
+		{
+			"empty authority",
+			types.MsgManageHookSubscription{
+				Authority: "",
+				HookSubscription: &types.HookSubscription{
+					ContractAddress: testutil.TestOwnerAddress,
+					Hooks:           []types.HookType{},
+				},
+			},
+			"authority is invalid: empty address string is not allowed",
+		},
+		{
+			"non unique hooks",
+			types.MsgManageHookSubscription{
+				Authority: "neutron1hxskfdxpp5hqgtjj6am6nkjefhfzj359x0ar3z",
+				HookSubscription: &types.HookSubscription{
+					ContractAddress: testutil.TestOwnerAddress,
+					Hooks:           []types.HookType{types.HookType_AfterValidatorBonded, types.HookType_AfterDelegationModified, types.HookType_AfterValidatorBonded},
+				},
+			},
+			"subscription hooks are not unique",
+		},
+		{
+			"good case",
+			types.MsgManageHookSubscription{
+				Authority: "neutron1hxskfdxpp5hqgtjj6am6nkjefhfzj359x0ar3z",
+				HookSubscription: &types.HookSubscription{
+					ContractAddress: testutil.TestOwnerAddress,
+					Hooks:           []types.HookType{},
+				},
+			},
+			"",
+		},
+	}
 
-	res, err := msgServer.ManageHookSubscription(ctx, &types.MsgManageHookSubscription{
-		Authority:        "",
-		HookSubscription: nil,
-	})
-	require.NoError(t, err)
-	require.NotEmpty(t, res)
+	for _, tt := range tests {
+		res, err := msgServer.ManageHookSubscription(ctx, &tt.manageHookSubscriptionMsg)
+
+		if tt.expectedErr == "" {
+			require.NoError(t, err, tt.expectedErr)
+			require.Equal(t, res, &types.MsgManageHookSubscriptionResponse{})
+		} else {
+			require.ErrorContains(t, err, tt.expectedErr)
+			require.Empty(t, res)
+		}
+	}
 }
