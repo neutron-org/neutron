ae915dd980aeaa977a998b6e1d0725e7f309de52
nhpd <dmitry.klpv@gmail.com>
2025-01-27 14:50:54 +0400

    add test for validate genesis state

--- x/harpoon/types/genesis_test.go
+++ x/harpoon/types/genesis_test.go
@@ -8,6 +8,8 @@ import (
 	"github.com/neutron-org/neutron/v5/x/harpoon/types"
 )
 
+const ContractAddress1 = "neutron159kr6k0y4f43dsrdyqlm9x23jajunegal4nglw044u7zl72u0eeqharq3a"
+
 func TestGenesisState_Validate(t *testing.T) {
 	tests := []struct {
 		desc     string
@@ -20,9 +22,52 @@ func TestGenesisState_Validate(t *testing.T) {
 			valid:    true,
 		},
 		{
-			desc:     "valid genesis state",
-			genState: &types.GenesisState{},
-			valid:    true,
+			desc: "valid genesis state",
+			genState: &types.GenesisState{
+				HookSubscriptions: []types.HookSubscriptions{
+					{
+						HookType:          types.HookType_AfterValidatorCreated,
+						ContractAddresses: []string{ContractAddress1},
+					},
+				},
+			},
+			valid: true,
+		},
+		{
+			desc: "empty address",
+			genState: &types.GenesisState{
+				HookSubscriptions: []types.HookSubscriptions{
+					{
+						HookType:          types.HookType_AfterValidatorCreated,
+						ContractAddresses: []string{""},
+					},
+				},
+			},
+			valid: false,
+		},
+		{
+			desc: "invalid address",
+			genState: &types.GenesisState{
+				HookSubscriptions: []types.HookSubscriptions{
+					{
+						HookType:          types.HookType_AfterValidatorCreated,
+						ContractAddresses: []string{"whatever"},
+					},
+				},
+			},
+			valid: false,
+		},
+		{
+			desc: "invalid hook",
+			genState: &types.GenesisState{
+				HookSubscriptions: []types.HookSubscriptions{
+					{
+						HookType:          types.HookType(-200),
+						ContractAddresses: []string{ContractAddress1},
+					},
+				},
+			},
+			valid: false,
 		},
 	}
 	for _, tc := range tests {
