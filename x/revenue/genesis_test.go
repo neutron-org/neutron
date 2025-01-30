13354c09c6472046ddbc8f65441eabd8539563a2
sotnikov-s <ssotnikov93@gmail.com>
2025-01-30 18:55:58 +0300

    implement UnpackInterfaces for GenesisState and add genesis import/export and serialisation tests

--- x/revenue/genesis_test.go
+++ x/revenue/genesis_test.go
@@ -1,27 +1,53 @@
 package revenue_test
 
-//func TestGenesis(t *testing.T) {
-//	k, ctx := keeper.CronKeeper(t, nil, nil)
-//
-//	genesisState := types.GenesisState{
-//		Params: types.DefaultParams(),
-//		ScheduleList: []types.Schedule{
-//			{
-//				Name:              "a",
-//				Period:            5,
-//				Msgs:              nil,
-//				LastExecuteHeight: uint64(ctx.BlockHeight()),
-//			},
-//		},
-//	}
-//
-//	cron.InitGenesis(ctx, *k, genesisState)
-//	got := cron.ExportGenesis(ctx, *k)
-//	require.NotNil(t, got)
-//
-//	nullify.Fill(&genesisState)
-//	nullify.Fill(got)
-//
-//	require.Equal(t, genesisState.Params, got.Params)
-//	require.ElementsMatch(t, genesisState.ScheduleList, got.ScheduleList)
-//}
+import (
+	"testing"
+
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/neutron-org/neutron/v5/testutil/revenue/keeper"
+	"github.com/neutron-org/neutron/v5/x/revenue"
+	revenuetypes "github.com/neutron-org/neutron/v5/x/revenue/types"
+	"github.com/stretchr/testify/require"
+)
+
+func TestInitAndExportGenesis(t *testing.T) {
+	k, ctx := keeper.RevenueKeeper(t, nil, nil, nil, "")
+
+	// create some non-default genesis state
+	genesisState := revenuetypes.DefaultGenesis()
+	genesisState.Validators = append(genesisState.Validators, revenuetypes.ValidatorInfo{
+		ConsensusAddress:            "neutronvalcons1arjwkww79m65csulawqngr7ngs4uqu5hv5736l",
+		CommitedBlocksInPeriod:      1000,
+		CommitedOracleVotesInPeriod: 1000,
+	})
+	genesisState.Params.PaymentScheduleType = revenuetypes.PAYMENT_SCHEDULE_TYPE_BLOCK_BASED
+	ps := &revenuetypes.BlockBasedPaymentSchedule{BlocksPerPeriod: 5, CurrentPeriodStartBlock: 1}
+	var err error
+	genesisState.State.PaymentSchedule, err = codectypes.NewAnyWithValue(ps)
+	require.Nil(t, err)
+
+	// apply genesis state, export it, and compare
+	revenue.InitGenesis(ctx, k, *genesisState)
+	got := revenue.ExportGenesis(ctx, k)
+	require.Equal(t, genesisState, got)
+}
+
+func TestGenesisSerialization(t *testing.T) {
+	registry := codectypes.NewInterfaceRegistry()
+	revenuetypes.RegisterInterfaces(registry)
+	cdc := codec.NewProtoCodec(registry)
+
+	genesisState := revenuetypes.DefaultGenesis()
+	data, err := cdc.MarshalJSON(genesisState)
+	require.NoError(t, err)
+	err = genesisState.Validate()
+	require.NoError(t, err)
+
+	var genesisState2 revenuetypes.GenesisState
+	err = cdc.UnmarshalJSON(data, &genesisState2)
+	require.NoError(t, err)
+
+	err = genesisState2.Validate()
+	require.NoError(t, err)
+}
