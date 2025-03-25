29d23f5b29a779cc1300f19fa01f72cf36c1d440
sotnikov-s <ssotnikov93@gmail.com>
2025-03-26 00:36:02 +0300

    make proto-gen auto formatting

--- x/interchainqueries/types/genesis.pb.go
+++ x/interchainqueries/types/genesis.pb.go
@@ -683,7 +683,7 @@ func (m *RegisteredQuery) Unmarshal(dAtA []byte) error {
 		switch fieldNum {
 		case 1:
 			if wireType != 0 {
-				return fmt.Errorf("proto: wrong wireType = %d for field ID", wireType)
+				return fmt.Errorf("proto: wrong wireType = %d for field Id", wireType)
 			}
 			m.Id = 0
 			for shift := uint(0); ; shift += 7 {
