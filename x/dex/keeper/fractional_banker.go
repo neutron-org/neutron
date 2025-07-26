1b032fd0b41e3648af8da1438dbaf6ed36b7348c
Julian Compagni Portis <jcompagni@gmail.com>
2025-07-25 19:29:25 -0600

    misc cleanup

--- x/dex/keeper/fractional_banker.go
+++ x/dex/keeper/fractional_banker.go
@@ -143,13 +143,11 @@ func RoundDownToWholeTokenAmounts(tokens types.PrecDecCoins) (wholeTokens sdk.Co
 	for _, token := range tokens {
 		wholeAmount := token.Amount.TruncateInt()
 		fractionalRemainder := token.Amount.Sub(math_utils.NewPrecDecFromInt(wholeAmount))
+
 		if !wholeAmount.IsZero() {
 			wholeTokens = wholeTokens.Add(sdk.Coin{Denom: token.Denom, Amount: wholeAmount})
-			fractionalDebts[token.Denom] = math_utils.ZeroPrecDec()
 		}
-		if !fractionalRemainder.IsZero() {
-			fractionalDebts[token.Denom] = fractionalRemainder
-		}
+		fractionalDebts[token.Denom] = fractionalRemainder
 	}
 
 	return wholeTokens, fractionalDebts
@@ -178,22 +176,4 @@ func CalcUserSendMinusDebts(amountToSend, debts types.PrecDecCoins) (sdk.Coins, 
 
 	}
 	return coinsToSend, debtMap
-}
-
-func RoundUpToWholeTokenAmounts(tokens types.PrecDecCoins) (wholeTokens sdk.Coins, fractionalTokens types.PrecDecCoins) {
-	wholeTokens = sdk.Coins{}
-	fractionalTokens = types.PrecDecCoins{}
-
-	for _, token := range tokens {
-		wholeAmount := token.Amount.Ceil().TruncateInt()
-		fractionalRemainder := math_utils.NewPrecDecFromInt(wholeAmount).Sub(token.Amount)
-		if !wholeAmount.IsZero() {
-			wholeTokens = append(wholeTokens, sdk.Coin{Denom: token.Denom, Amount: wholeAmount})
-		}
-		if !fractionalRemainder.IsZero() {
-			fractionalTokens = append(fractionalTokens, types.PrecDecCoin{Denom: token.Denom, Amount: fractionalRemainder})
-		}
-	}
-
-	return wholeTokens, fractionalTokens
 }
