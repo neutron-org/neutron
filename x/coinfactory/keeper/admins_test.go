453bc5f8c2293e494b89b6f5088d0f2c8945049b
Dev <programmer10110@gmail.com>
2025-11-01 15:34:46 +0200

    Update x/coinfactory/keeper/admins_test.go
    Co-authored-by: sotnikov-s <34917380+sotnikov-s@users.noreply.github.com>

--- x/coinfactory/keeper/admins_test.go
+++ x/coinfactory/keeper/admins_test.go
@@ -85,7 +85,7 @@ func (suite *KeeperTestSuite) TestMintDenom() {
 		{
 			desc:      "denom does not exist",
 			amount:    10,
-			mintDenom: "factory/osmo1t7egva48prqmzl59x5ngv4zx0dtrwewc9m7z44/evmos",
+			mintDenom: "factory.osmo1t7egva48prqmzl59x5ngv4zx0dtrwewc9m7z44.evmos",
 			admin:     suite.TestAccs[0].String(),
 			valid:     false,
 		},
