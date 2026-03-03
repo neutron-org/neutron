888e71cafbd4dc4cded415fa05a68fff06615442
pr0n00gler <programmer10110@gmail.com>
2026-03-03 15:28:50 +0200

    fix migration

--- app/upgrades/nextupgrade/types.go
+++ app/upgrades/nextupgrade/types.go
@@ -1,8 +1,8 @@
 package nextupgrade
 
 type IBCRateLimitsExecuteMessage struct {
-	GrantRole  *GrantRole  `json:"grant_role"`
-	RevokeRole *RevokeRole `json:"revoke_role"`
+	GrantRole  *GrantRole  `json:"grant_role,omitempty"`
+	RevokeRole *RevokeRole `json:"revoke_role,omitempty"`
 }
 
 type GrantRole struct {
