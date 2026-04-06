7a032e2b1494c2a56019f100b71bb984dbbb263c
pr0n00gler <programmer10110@gmail.com>
2026-04-06 13:39:41 +0300

    refactoring

--- x/dex/migrations/v8/store.go
+++ x/dex/migrations/v8/store.go
@@ -8,7 +8,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 
 	"github.com/neutron-org/neutron/v10/x/dex/types"
-	v7types "github.com/neutron-org/neutron/v10/x/dex/types/v7"
 )
 
 // MigrateStore performs in-place store migrations.
@@ -29,7 +28,7 @@ func migrateParams(ctx sdk.Context, cdc codec.BinaryCodec, storeKey storetypes.S
 	if bz == nil {
 		return errors.New("cannot fetch dex params from KV store")
 	}
-	var oldParams v7types.Params
+	var oldParams Params
 	cdc.MustUnmarshal(bz, &oldParams)
 
 	// add new param values
--- x/dex/types/v7/params.go
+++
@@ -1,9 +0,0 @@
-package v7
-
-import "gopkg.in/yaml.v2"
-
-// String implements the Stringer interface.
-func (p Params) String() string {
-	out, _ := yaml.Marshal(p)
-	return string(out)
-}
