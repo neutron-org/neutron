828104ba4c085df630e3654028cec41f3e7078e5
swelf <golishkin@gmail.com>
2026-04-24 15:42:25 +0300

    clear voting registry

--- app/upgrades/nextupgrade/types.go
+++ app/upgrades/nextupgrade/types.go
@@ -14,3 +14,20 @@ type RevokeRole struct {
 	Signer string   `json:"signer"`
 	Roles  []string `json:"roles"`
 }
+
+type VotingRegistryExecuteMessage struct {
+	DeactivateVotingVault *DeactivateVotingVault `json:"deactivate_voting_vault,omitempty"`
+	UpdateConfig          *UpdateConfig          `json:"update_config,omitempty"`
+}
+
+type DeactivateVotingVault struct {
+	VotingVaultContract string `json:"voting_vault_contract"`
+}
+
+type UpdateConfig struct {
+	Owner string `json:"owner"`
+}
+
+type VotingVault struct {
+	Address string `json:"address"`
+}
